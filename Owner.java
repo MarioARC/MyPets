7bcfd5021bf10370f444536a7a10a3347795e03e
Mario Castelo <91093467@g.mvusd.net>
2021-02-17 20:09:17 +0000

    Added an owner Emiliano

---
+++ Owner.java
@@ -0,0 +1,19 @@
+public class Owner
+{
+  private String name;
+
+  public Owner(String name)
+  {
+    this.name = name;
+  }
+
+  public String feed(PetParent pet)
+  {
+    return "Time to feed " + pet.getName() + ".";
+  }
+
+  public String toString()
+  {
+    return "I am " + this.name;
+  }
+}
